11db5b6b5bb908d3980b1d274c7a7642a6811d81
nirsancho <nirsancho@gmail.com>
2014-10-24 12:54:34 +0300

    change phonegap test

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var app = (function ($, app, console, document) {
     app.lang = "es";
     app.vars = {};
     app.isPhonegap = (function () {
+        return document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
         return navigator === undefined ? false : navigator.hasOwnProperty("notification");
     })();
 
