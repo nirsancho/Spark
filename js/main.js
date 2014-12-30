4c6d2a8114209d96471151b3af5b4d6c18a9dcc9
Nir Sancho <nirsancho@gmail.com>
2014-12-30 22:32:50 +0200

    net stat

--- js/main.js
+++ js/main.js
@@ -3,13 +3,21 @@ console.log(app)
 
 function checkConnection() {
     app.log("checkConnection()");
-    if (app && app.isPhonegap) {
+    if (app && app.isPhonegap && navigator && navigator.connection.type) {
 
-        if (navigator.onLine) {
-            alert("online");
-        } else {
-            alert("offline");
-        }
+        var networkState = navigator.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN] = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI] = 'WiFi connection';
+        states[Connection.CELL_2G] = 'Cell 2G connection';
+        states[Connection.CELL_3G] = 'Cell 3G connection';
+        states[Connection.CELL_4G] = 'Cell 4G connection';
+        states[Connection.CELL] = 'Cell generic connection';
+        states[Connection.NONE] = 'No network connection';
+
+        alert('Connection type: ' + states[networkState]);
     }
 }
 
