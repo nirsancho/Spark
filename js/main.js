d7cb55bef2f0e2762fad3e894b3bda68761eb2e9
Nir Sancho <nirsancho@gmail.com>
2014-12-30 22:23:19 +0200

    f

--- js/main.js
+++ js/main.js
@@ -4,19 +4,12 @@ console.log(app)
 function checkConnection() {
     app.log("checkConnection()");
     if (app && app.isPhonegap) {
-        var networkState = navigator.connection.type;
 
-        var states = {};
-        states[Connection.UNKNOWN] = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI] = 'WiFi connection';
-        states[Connection.CELL_2G] = 'Cell 2G connection';
-        states[Connection.CELL_3G] = 'Cell 3G connection';
-        states[Connection.CELL_4G] = 'Cell 4G connection';
-        states[Connection.CELL] = 'Cell generic connection';
-        states[Connection.NONE] = 'No network connection';
-
-        alert('Connection type: ' + states[networkState]);
+        if (navigator.onLine) {
+            alert("online");
+        } else {
+            alert("offline");
+        }
     }
 }
 
