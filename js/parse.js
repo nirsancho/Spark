6f96dfc47c5ed11c1be86be50c6252f62976d153
Nir Sancho <nirsancho@gmail.com>
2014-12-30 23:37:32 +0200

    adding email to event

--- js/parse.js
+++ js/parse.js
@@ -134,7 +134,7 @@ app = (function ($, app, document) {
             $("[data-role=page-title]", $html).html(page_form.title);
             $("[data-role=content]", $html).prepend(page_form.body);
             $("[data-text=general-send]", $html).on("click", function (e) {
-                app.ga.trackEvent(app.log, app.log, "App", "Form Sent", "tel: " + $("#form-tel").val() +", code: " + $("#form-coupon").val() , 0);
+                app.ga.trackEvent(app.log, app.log, "App", "Form Sent", "email: " + $("#form-email").val() + "tel: " + $("#form-tel").val() +", code: " + $("#form-coupon").val() , 0);
                     var form_data = {
                         appId: 276960,
                         ownerEmail: "sancho@sefarad.com"
